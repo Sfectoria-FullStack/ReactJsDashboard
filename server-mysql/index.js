c17a371fcabc0b065dbf5fe347cccf02ccf052bf
khalil kraiem <khalilmedkraiem@gmail>
2024-03-04 16:47:40 +0100

    products router in index

--- server-mysql/index.js
+++ server-mysql/index.js
@@ -5,7 +5,7 @@ require('./models/index')
 const cors = require("cors");
 const userRouter = require("./router/users.router");
 app.use(cors({origin:"*"}))
-// const productRouter = require("./router/products.router");
+const productRouter = require("./router/products.router");
 
 app.use(
   cors({
@@ -13,7 +13,7 @@ app.use(
   })
 );
 
-// app.use('/products',productRouter)
+app.use('/products',productRouter)
 app.use('/users',userRouter)
 
 app.listen(PORT, () => {
