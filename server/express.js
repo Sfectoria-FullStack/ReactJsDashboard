f31b741779aa51c94e87e8f574a0781b6043b902
Mestiri77 <faroukmestiri77@gmail.com>
2024-03-08 14:25:25 +0100

    express-swaggerUi

--- server/express.js
+++ server/express.js
@@ -1,10 +1,37 @@
 const express = require("express");
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const productRouter = require("./router/products.router");
+const swaggerJsdoc = require("swagger-jsdoc");
 require("./models/index");
 
 const app = express();
 app.use(express.json());
 app.use("/products", productRouter);
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Sfectoria Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      contact: {
+        name: "Sfectoria",
+        url: "https://www.sfectoria.com",
+        email: "contact@sfectoria.com",
+      },
+    }
+  },
+  apis: ["./router/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
 app.listen(4000, () => {
   console.log("your server is listening to http://127.0.0.1:4000");
--- server/router/products.router.js
+++ server/router/products.router.js
@@ -3,7 +3,64 @@ const { selectAllProducts, createProduct, getOne, removeProduct } = require("../
 const productRouter=express.Router()
 
 
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Retrieve a list of all products.
+ *     description: Retrieve a list of products.
+ *     tags: 
+ *        - products
+ *     responses:
+ *       200:
+ *         description: A list of products.
+ *         
+ */
 productRouter.get("/",selectAllProducts) 
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     summary: Create a  user.
+ *     tags: 
+ *        - products
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       description: The product ID.
+ *                       example: 1
+ *                     productName:
+ *                       type: string
+ *                       description: The product name.
+ *                       example: iphone14
+ *                     imageURL:
+ *                       type: string
+ *                       description: The product image.
+ *                       example: www.sfectoria.com/react.jpg
+ *                     price:
+ *                       type: integer
+ *                       description: The product's price.
+ *                       example: 130
+ *                     shortDescription:String:
+ *                       type: string
+ *                       description: The product image.
+ *                       example: www.sfectoria.com/react.jpg
+ *                     longDescription:String:
+ *                       type: string
+ *                       description: The product image.
+ *                       example: www.sfectoria.com/react.jpg
+ * 
+*/
 productRouter.post ("/",createProduct)
 productRouter.get("/one/:id",getOne)
 productRouter.delete("/:id",removeProduct)
